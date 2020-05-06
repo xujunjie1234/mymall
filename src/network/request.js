0b2f2836c620236b13100315ad6eb0c3d1b7146f
junjie <1936725526@qq.com>
2020-05-06 13:21:33 +0800

    new request api

--- src/network/request.js
+++ src/network/request.js
@@ -4,8 +4,7 @@ export function request(config, success, failure) {
     //1创建实例
     const instance = axios.create({
         //具体接口请加老师微信问老师要
-        // baseURL: "http://123.207.32.32:8000/api/h8",
-        baseURL: "http://152.136.185.210:8000/api/n3",
+        // baseURL: "http://123.207.32.32:8000/",
         timeout: 5000
     })
 
